997293554a7168b5c89d0ec9d22a8ed703c18970
Cyopn <frijolesconcatsu@gmail.com>
2021-09-23 16:39:26 -0500

    Clase principal

---
+++ index.js
@@ -0,0 +1,48 @@
+const { create, Client } = require("@open-wa/wa-automate")
+const welcome = require('./lib/welcome')
+const handler = require("./handler")
+const opt = require("./options")
+const { prefix } = require("./config")
+
+const start = async(client = new Client()) => {
+    console.log("[SR] Cliente listo")
+    client.onStateChanged((state) => {
+        console.log("[SR]", state)
+        if (state === 'CONFLICT' || state === 'UNLAUNCHED') {
+            client.forceRefocus()
+        }
+    })
+
+    client.onMessage((async(message) => {
+        handler(client, message)
+    }))
+
+    client.onGlobalParticipantsChanged((async(event) => {
+        await welcome(client, event)
+    }))
+
+    client.onAddedToGroup(((chat) => {
+        let tMem = chat.groupMetadata.participants.length
+        if (tMem < 5) {
+            client.sendText(chat.id, `Para usarme necesita al menos 5 miembros en el grupo, total de miembros : ${tMem}`)
+                .then(() => {
+                    setTimeout(() => {
+                        client.leaveGroup(chat.id)
+                    }, 3000)
+                })
+        } else {
+            client.sendText(`Hola, *${chat.contact.name}*, gracias por invitarme, escribe *${prefix}help* para ver el menu de comandos`)
+        }
+    }))
+
+    client.onIncomingCall((async(call) => {
+        await client.sendText(call.peerJid, "Llamar= Bloquear\nPara arreglar esto contactame\nwa.me/+525627127780")
+            .then(() => {
+                client.contactBlock(call.peerJid)
+            })
+    }))
+}
+
+create(opt(true, start))
+    .then(client => start(client))
+    .catch((err) => console.log("[SR]", err))
