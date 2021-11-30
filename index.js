00de63caba2b06c3a77c6126af5548e60038b1b9
Cyopn <frijolesconcatsu@gmail.com>
2021-11-30 02:04:11 -0600

    Clase principal

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const { create, Client } = require("@open-wa/wa-automate")
 const welcome = require('./lib/welcome')
 const handler = require("./handler")
-const opt = require("./options")
+const opt=require("./options")
 const { prefix } = require("./config")
 
 const start = async(client = new Client()) => {
     console.log("[SR] Cliente listo")
-    client.onStateChanged((state) => {
+    await client.onStateChanged((state) => {
         console.log("[SR]", state)
         if (state === 'CONFLICT' || state === 'UNLAUNCHED') {
             client.forceRefocus()
         }
     })
 
-    client.onMessage((async(message) => {
+    await client.onMessage((async (message) => {
         await handler(client, message)
     }))
 
-    client.onGlobalParticipantsChanged((async(event) => {
+    await client.onGlobalParticipantsChanged((async (event) => {
         await welcome(client, event)
     }))
 
-    client.onAddedToGroup(((chat) => {
+    await client.onAddedToGroup(((chat) => {
         let tMem = chat.groupMetadata.participants.length
         if (tMem < 5) {
             client.sendText(chat.id, `Para usarme necesita al menos 5 miembros en el grupo, total de miembros : ${tMem}`)
@@ -35,13 +35,35 @@ const start = async(client = new Client()) => {
         }
     }))
 
-    client.onIncomingCall((async(call) => {
+    await client.onIncomingCall((async (call) => {
         await client.sendText(call.peerJid, "Llamar= Bloquear\nPara arreglar esto contactame\nwa.me/+525627127780")
             .then(() => {
                 client.contactBlock(call.peerJid)
             })
     }))
 }
+
+/*create({
+    sessionId:"Cyopn",
+    headless:true,
+    qrTimeout:0,
+    authTimeout:0,
+    cacheEnabled:false,
+    useChrome:true,
+    killProcessOnBrowserClose:true,
+    throwErrorOnTosBlock:true,
+    chromiumArgs:[
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--aggressive-cache-discard',
+        '--disable-cache',
+        '--disable-application-cache',
+        '--disable-offline-load-stale-cache',
+        '--disk-cache-size=0'
+    ],
+    onError:'AS_STRING'})
+    .then(client => start(client))
+    .catch(e => console.log("[SR]", e))*/
 
 create(opt(true, start))
     .then(client => start(client))
