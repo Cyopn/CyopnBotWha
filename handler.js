3345b60fb2be720c11327786efa2bcbeb1447bcc
Cyopn <frijolesconcatsu@gmail.com>
2021-12-07 00:17:19 -0600

    Handler

--- handler.js
+++ handler.js
@@ -8,7 +8,7 @@ fs.readdir('./commands/', (err, files) => {
     if (err) return console.error(err)
     let jsfile = files.filter(f => f.split(".").pop() === "js")
     if (jsfile.length <= 0) return console.log("No se encontro ningun comando");
-    jsfile.forEach((f, i) => {
+    jsfile.forEach((f) => {
         let pull = require(`./commands/${f}`)
         command.push(pull.config.name)
         alias.push(pull.config.aliases)
