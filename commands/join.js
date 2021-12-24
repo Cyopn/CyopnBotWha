ef4bcb724c0774e0a1d51404fdc84b2c1b81f113
Cyopn <frijolesconcatsu@gmail.com>
2021-12-24 12:10:57 -0600

    Comandos

--- commands/join.js
+++ commands/join.js
@@ -1,15 +1,27 @@
 module.exports.run = async(client, message, args, config) => {
     const { type, id, from, t, sender, author, isGroupMsg, chat, chatId, caption, isMedia, mimetype, quotedMsg, quotedMsgObj, mentionedJidList } = message
+    const arg = args.join("")
+    const isUrl = arg.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
     try {
-
+        if (!arg) {
+            await client.reply(from, `Envia el comando ${config.prefix}join [invitacion]`, id)
+        } else {
+            if (!isUrl) {
+                await client.reply(from, `Envia un enlace de invitacion valido`, id)
+            } else {
+                await client.joinGroupViaLink(arg, { returnChatObj: true }).then(a => {
+                    client.sendText(a.id, `Gracias por invitarme, usa ${config.prefix}help para conocer los comandos`)
+                })
+            }
+        }
     } catch (e) {
         console.error(e)
-        await client.reply(from, `Ocurrio un error`, id)
+        await client.reply(from, ` Ocurrio un error `, id)
     }
 }
 
 module.exports.config = {
-    name: "",
-    aliases: '',
-    desc: ''
+    name: "join",
+    aliases: 'j',
+    desc: 'Añade el bot a un grupo con solo un comando'
 }
--- commands/telest.js
+++ commands/telest.js
@@ -1,18 +1,31 @@
-const axios=require("axios")
+const axios = require("axios")
 module.exports.run = async(client, message, args, config) => {
     const { id, from } = message
-    const { prefix, zeeksKey }=config
+    const { prefix, zeeksKey } = config
     try {
-        if(!args)return await client.reply(from, `Envia el comando *${prefix}telest [url], la url debe ser directo al paquete de stickers. \nEs recomendable no usar el comando en grupos, para evitar spam`, id)
-        
-        const response=await axios.get(`https://api.zeks.me/api/telegram-sticker?apikey=${zeeksKey}&url=${args.join("")}`)
-        const res=response.data.result
-        
-        if(res===undefined){
-            await client.reply(from, `Ha ocurrido un error, asegurate que el paquetede stickers NO sean animados`, id)
+
+        if (args) {
+            const response = await axios.get(`https://api.zeks.me/api/telegram-sticker?apikey=${zeeksKey}&url=${args.join("")}`)
+            const res = response.data.result
+
+            if (res === undefined) {
+                await client.reply(from, `Ha ocurrido un error, asegurate que el paquetede stickers NO sean animados`, id)
+            } else {
+                await client.reply(from, `Enviando ${res.length} stickers`, id)
+                res.forEach(r => {
+                    client.sendStickerfromUrl(from, r, { method: 'get' }, {
+                        author: 'ig: @Cyopn_',
+                        pack: 'CyopnBot'
+                    })
+                });
+                3
+            }
+
+        } else {
+            await client.reply(from, `Envia el comando *${prefix}telest [url], la url debe ser directo al paquete de stickers. \nEs recomendable no usar el comando en grupos, para evitar spam`, id)
         }
-        
-        
+
+
     } catch (e) {
         console.error(e)
         await client.reply(from, `Ocurrio un error`, id)
