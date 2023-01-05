0bcf5a42cb2cd6e25fae9b90785b476e28603d39
Cyopn <frijolesconcatsu@gmail.com>
2023-01-05 03:17:34 -0600

    Comandos
    Descarga de audio y busqueda de canciones

--- commands/play.js
+++ commands/play.js
@@ -1,36 +1,94 @@
 const axios = require('axios')
-const ytsr=require("ytsr")
+const { youtubeDl } = require('../lib/functions')
 
-module.exports.run = async(client, message, args, config) => {
+module.exports.run = async (client, message, args, config) => {
     const { id, from } = message
-    const isUrl = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/gi)
-    const { zeeksKey, prefix } = config
     const arg = args.join('')
+    //const isUrl = arg.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/gi)
+    const isUrl = arg.match(/^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/img)
 
     try {
         if (!arg) return await client.reply(from, `Envia el comando *${prefix}play [consulta/url]*`, id)
         await client.reply(from, `Espera un momento`, id)
-        if (isUrl && arg.startsWith('www.youtu')) {
-            const res = await axios.get(`https://api.zeks.me/api/ytmp3/2?apikey=${zeeksKey}&url=${as.url}?v`)
-            let ytm = res.data.result
-            if (res.status === false) await client.reply(res.data.message)
-            await client.sendFileFromUrl(from, `${ytm.thumb}`, `${ytm.title}.jpg`, `Inicia la descarga de *${ytm.title}*\nTamaño: ${ytm.size}\nCalidad: ${ytm.quality}`)
-            await client.sendFileFromUrl(from, `${ytm.link}`, `${ytm.title}.mp3`, "")
+        if (isUrl) {
+            let r = await youtubeDl([arg, config.fgmKey])
+            if (r == false) {
+                await client.reply(from, `El servicio no esta disponible`, id)
+            } else {
+                await client.sendFileFromUrl(from, `${r.thumb}`, `a.jpg`, `Inicia la descarga de *${r.title}*\nTamaño: ${r.size}\nCanal/Autor: ${r.channel}`, id)
+                await client.sendFileFromUrl(from, `${r.result}`, `${r.title}.mp3`, "", id)
+            }
         } else {
-            const a = await ytsr(arg, { limit: 1 })
-            const as = a.items[0]
-            const res = await axios.get(`https://api.zeks.me/api/ytmp3/2?apikey=${zeeksKey}&url=${as.url}?v`)
+            const res = await axios.get(`https://api-fgmods.ddns.net/api/ytsearch?q=${args.join(" ")}&apikey=${config.fgmKey}`)
             let ytm = res.data.result
-            if (res.status === false) await client.reply(res.data.message)
-            await client.sendFileFromUrl(from, `${ytm.thumb}`, `${ytm.title}.jpg`, `Inicia la descarga de *${ytm.title}*\nTamaño: ${ytm.size}\nCalidad: ${ytm.quality}`)
-            await client.sendFileFromUrl(from, `${ytm.link}`, `${ytm.title}.mp3`, "")
+            let rs
+            if (!ytm || ytm == undefined) {
+                await client.reply(from, `No se encontraron resultados para tu busqueda`, id)
+            } else {
+                a = 1
+                txt = "     Resultados"
+                ytm.forEach(rs => {
+                    if (a <= 10 && rs.type == "video") {
+                        txt += `
+${a}-. Titulo: ${rs.title}
+Duracion: ${rs.timestamp}
+Autor/Canal: ${rs.author.name}
+`
+                        a += 1
+                    } else {
+                        return
+                    }
+                });
+
+                await client.reply(from, txt += "Responde este mensaje con la opcion a descargar\nEl tiempo de espera es de solo un minuto", id)
+                const filter = message => message.author
+                client.awaitMessages(from, filter, { max: 1, time: 60000, errors: ['time', 'max'] }).then(c => {
+                    let d
+                    c.forEach(r => {
+                        d = r
+                    });
+                    if (d.quotedMsg == null) {
+                        client.reply(from, `Debes reponder al mensaje indicado
+Intenta de nuevo`, id)
+                    } else {
+                        if (d.quotedMsg.body.includes("Responde este mensaje con la opcion a descargar")) {
+                            if (!isNaN(d.body)) {
+                                if (d.body > 10) {
+                                    client.reply(from, `El numero excede al de las opciones
+Intenta de nuevo`)
+                                } else {
+                                    rs = parseInt(d.body)
+                                    youtubeDl([ytm[rs-1].url, config.fgmKey]).then(rss => {
+                                        if (rss == false) {
+                                            client.reply(from, `El servicio no esta disponible`, id)
+                                        } else {
+                                            client.sendFileFromUrl(from, `${rss.thumb}`, `a.jpg`, `Inicia la descarga de *${rss.title}*\nTamaño: ${rss.size}\nCanal/Autor: ${rss.channel}`, id)
+                                            client.sendFileFromUrl(from, `${rss.result}`, `${rss.title}.mp3`, "", id)
+                                        }
+                                     })
+
+                                }
+                            } else {
+                                console.log("w")
+                                client.reply(from, `No de admiten caracteres
+Intenta de nuevo`)
+                            }
+                        } else {
+                            client.reply(from, `Debes reponder al mensaje indicado
+Intenta de nuevo`, id)
+                        }
+                    }
+                }).catch(e => {
+                    console.log(e)
+                })
+            }
         }
-
     } catch (e) {
-        console.error(e)
+        console.error(e.toString())
         await client.reply(from, `Ocurrio un error`, id)
-    }
 
+    }
+    await client.simulateTyping(from, false)
 }
 
 module.exports.config = {
