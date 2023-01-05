cb272d2863d8fd36b60c479119efe311efdfd79c
Cyopn <frijolesconcatsu@gmail.com>
2023-01-05 03:18:48 -0600

    Comandos
    Optimizacion en el proceso para crear stickers

--- commands/sticker.js
+++ commands/sticker.js
@@ -1,44 +1,32 @@
 const { decryptMedia } = require('@open-wa/wa-decrypt')
-const { createSt } = require('../lib/functions')
-const fs = require('fs')
 
 module.exports.run = async(client, message, args, config) => {
     const { type, id, from, isMedia, quotedMsg } = message
-    const isUrl = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/gi)
-    const uaOverride = 'WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
-
+    
     try {
         if (isMedia && type === 'image') {
-            const mediaData = await decryptMedia(message, uaOverride)
-            fs.writeFile('./media/images/imgSt.jpg', mediaData, function(err) {
-                if (err) {
-                    return console.error(err);
-                }
-            });
 
-            await createSt(client, message)
+            const mediaData = await decryptMedia(message);
+            const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString(
+                'base64'
+            )}`;
+            client.sendImageAsSticker(from, imageBase64, {
+                author: 'ig: @Cyopn_',
+                pack: 'CyopnBot',
+                keepScale: true
+            })
 
         } else if (quotedMsg && quotedMsg.type === 'image') {
-            const mediaData = await decryptMedia(quotedMsg, uaOverride)
-            fs.writeFile('./media/images/imgSt.jpg', mediaData, function(err) {
-                if (err) {
-                    return console.error(err);
-                }
-            });
 
-            await createSt(client, message)
-
-        } else if (args.length >= 1) {
-            const url = args.join('')
-            if (url.match(isUrl)) {
-                await client.sendStickerfromUrl(from, url, { method: 'get' }, {
-                        author: 'ig: @Cyopn_',
-                        pack: 'CyopnBot'
-                    })
-                    .catch(err => console.error(err))
-            } else {
-                await client.reply(from, `El enlace no es valido`, id)
-            }
+            const mediaData = await decryptMedia(quotedMsg);
+            const imageBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString(
+                'base64'
+            )}`;
+            client.sendImageAsSticker(from, imageBase64, {
+                author: 'ig: @Cyopn_',
+                pack: 'CyopnBot',
+                keepScale: true
+            })
         } else {
             await client.reply(from, `Envia una imagen con el comando *${config.prefix}sticker*, responde a una imagen ya enviada o un link`, id)
         }
