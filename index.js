e862344f1819b58ab39e288c09df9955c91fe24b
Cyopn <frijolesconcatsu@gmail.com>
2023-03-09 01:15:42 -0600

    Script principal
    Evento de bienvenida poco util
    Mensaje de bienvenida (evento)

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { create, Client } = require("@open-wa/wa-automate")
-const { welcome } = require("./lib/functions")
 const handler = require("./handler")
 const temp = require("./media/temp/output.json")
 
@@ -13,20 +12,35 @@ const start = async (client = new Client()) => {
         }
     })
 
-    await client.onMessage((async (message) => {
+    await client.onMessage(async (message) => {
         await handler(client, message)
-    }))
+    })
 
-    await client.onGlobalParticipantsChanged((async (event) => {
-        await welcome(client, event)
-    }))
+    await client.onAddedToGroup(async (chat) => {
+        let tMem = chat.groupMetadata.participants.length
+        if (tMem < 5) {
+            client.sendText(chat.id, `Para usarme necesita al menos 5 miembros en el grupo, total de miembros : ${tMem}`)
+                .then(() => {
+                    setTimeout(() => {
+                        client.leaveGroup(chat.id)
+                    }, 3000)
+                })
+        } else {
+            client.sendText(chat.groupMetadata.id, `Gracias por la invitacion
+Puedes escribir ${config.prefix}help para ver los comandos
+Para resolver tus dudas sobre el desarrollo del bot, puedes contactarme aqui:
+WhatsApp: wa.me/525633592644
+Discord: Cyopn#7302
+Instagram: https://instagram.com/Cyopn_`)
+        }
+    })
 
-    await client.onIncomingCall((async (call) => {
+    await client.onIncomingCall(async (call) => {
         await client.sendText(call.peerJid, "Llamar = Bloquear\nPara arreglar esto contactame\nwa.me/+525627127780")
             .then(() => {
                 client.contactBlock(call.peerJid)
             })
-    }))
+    })
 }
 
 create({
