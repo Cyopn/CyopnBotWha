58f5ca9bd191bff4b8361c987a05f71ad409fd39
Cyopn <2021452176@teschi.edu.mx>
2023-04-22 02:18:41 -0600

    Comandos
    Descarga de facebook arreglado

--- commands/fbdownload.js
+++ commands/fbdownload.js
@@ -1,92 +1,75 @@
 const fbdown = require("fbdown");
 module.exports.run = async (client, message, args, config) => {
-  const { id, from } = message;
-  const { prefix } = config;
+	const { id, from } = message;
+	const { prefix } = config;
 
-  await client.reply(from, `Espera un poco`, id);
-  if (!args.join("")) {
-    await client.reply(from, `Usa *${prefix}fbdl [enlace]*`, id);
-  } else {
-    const arg = args[0];
-    const isUrl = arg.match(
-      /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi
-    );
-    if (isUrl) {
-      const res = await fbdown(arg);
-      if (res.includes("Error establishing a database connection"))
-        return client.reply(
-          from,
-          `No se pudo establecer conexion con el servidor
-Intenta mas tarde`,
-          id
-        );
-      if (res.medias === undefined)
-        return client.reply(from, `El enlace no es valido`, id);
-      if (res.medias[1] != undefined) {
-        await client
-          .sendFile(from, res.medias[1].url, "nose", `w`, id)
-          .catch((e) => {
-            if (
-              e
-                .toString()
-                .includes("Error: Evaluation failed: Error: MediaFileTooLarge:")
-            ) {
-              client
-                .sendFile(from, res.medias[0].url, "nose", `w`, id)
-                .catch((e) => {
-                  console.error(
-                    `Error en ${this.config.name}
+	await client.reply(from, `Espera un poco`, id);
+	if (!args.join("")) {
+		await client.reply(from, `Usa *${prefix}fbdl [enlace]*`, id);
+	} else {
+		const arg = args[0];
+		const isUrl = arg.match(/www.facebook.com|fb.watch/g);
+		if (isUrl) {
+			const res = await fbdown(arg);
+			if (res.url === undefined)
+				return client.reply(from, `El enlace no es valido`, id);
+			if (res.url[0] != undefined) {
+				await client
+					.sendFile(from, res.url[0].url, "nose", `w`, id)
+					.catch((e) => {
+						if (
+							e
+								.toString()
+								.includes(
+									"Error: Evaluation failed: Error: MediaFileTooLarge:"
+								)
+						) {
+							client
+								.sendFile(
+									from,
+									res.url[1].url,
+									"nose",
+									`w`,
+									id
+								)
+								.catch((e) => {
+									console.error(
+										`Error en ${this.config.name}
 Hora: ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}:`,
-                    e.toString()
-                  );
-                  client.reply(from, `Es imposible enviar el video`, id);
-                });
-            } else {
-              console.error(
-                `Error en ${this.config.name}
+										e.toString()
+									);
+									client.reply(
+										from,
+										`Es imposible enviar el video`,
+										id
+									);
+								});
+						} else {
+							console.error(
+								`Error en ${this.config.name}
 Hora: ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}:`,
-                e.toString()
-              );
-              client.reply(from, `Es imposible enviar el video`, id);
-            }
-          });
-      } else if (
-        res.medias[1] === undefined &&
-        res.medias[0].url != undefined
-      ) {
-        await client
-          .sendFile(from, res.medias[0].url, "nose", `w`, id)
-          .catch((e) => {
-            if (
-              e
-                .toString()
-                .includes("Error: Evaluation failed: Error: MediaFileTooLarge:")
-            ) {
-              client
-                .sendFile(from, res.medias[0].url, "nose", `w`, id)
-                .catch((e) => {
-                  console.error(
-                    `Error en ${this.config.name}
-Hora: ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}:`,
-                    e.toString()
-                  );
-                  client.reply(from, `Es imposible enviar el video`, id);
-                });
-            }
-          });
-      } else {
-        client.reply(from, `EL video fue eliminado o es privado`, id);
-      }
-    } else {
-      await client.reply(from, `El enlace no es valido`, id);
-    }
-  }
+								e.toString()
+							);
+							client.reply(
+								from,
+								`Es imposible enviar el video`,
+								id
+							);
+						}
+					});
+			}else {
+				client.reply(from, `EL video fue eliminado o es privado`, id);
+			}
+		} else {
+			await client.reply(from, `El enlace no es valido`, id);
+		}
+	}
 
-  await client.simulateTyping(from, false);
+	await client.simulateTyping(from, false);
 };
 
 module.exports.config = {
-  name: "fbdownload",
-  aliases: "fbdl",
-  desc: "Obtién multimedia de una publicacion de facebook",
+	name: "fbdownload",
+	aliases: "fbdl",
+	desc: "Obtién multimedia de una publicacion de facebook",
 };
