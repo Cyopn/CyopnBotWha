7d31590659aa7738e3478653ed90ad33f0f8d2e0
Cyopn <2021452176@teschi.edu.mx>
2023-04-24 23:39:39 -0600

    Clase principal - Manejador
    Scripts con estructura limpia

--- handler.js
+++ handler.js
@@ -1,50 +1,41 @@
-const fs = require('fs')
-let command = []
-let alias = []
-const config = require('./config.json')
-const { getAfk, lvlFunc } = require("./lib/functions")
-const temp = require("./media/temp/output.json")
+const fs = require("fs");
+let command = [];
+let alias = [];
+const config = require("./config.json");
+const { lvlFunc, getAfk } = require("./lib/functions");
 
-fs.readdir('./commands/', (err, files) => {
+fs.readdir("./commands/", (err, files) => {
+	if (err) return console.error(err);
+	let jsfile = files.filter((f) => f.split(".").pop() === "js");
+	if (jsfile.length <= 0) return console.log("No se encontro ningun comando");
+	jsfile.forEach((f) => {
+		let pull = require(`./commands/${f}`);
+		command.push(pull.config.name);
+		alias.push(pull.config.alias);
+	});
+});
 
-    if (err) return console.error(err)
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if (jsfile.length <= 0) return console.log("No se encontro ningun comando");
-    jsfile.forEach((f) => {
-        let pull = require(`./commands/${f}`)
-        command.push(pull.config.name)
-        alias.push(pull.config.aliases)
-    });
-})
-
-module.exports = msgHandler = async (client, message) => {
-    const { type, id, from, caption, mentionedJidList } = message
-    let { body } = message
-    body = (type === 'chat' && body.startsWith(config.prefix)) ? body : ((type === 'image' && caption || type === 'video' && caption) && caption.startsWith(config.prefix)) ? caption : ''
-
-    try {
-        await lvlFunc(client, message)
-
-        if (mentionedJidList && mentionedJidList[0]) {
-            await getAfk(client, message)
-        }
-        if (body.startsWith(config.prefix)) {
-
-            const args = body.slice(config.prefix.length).trim().split(' ')
-            const comm = args.shift().toLowerCase()
-            const sr = command.indexOf(comm) === -1 ? alias.indexOf(comm) : command.indexOf(comm)
-            if (sr === -1) {
-                return
-            } else {
-                await client.simulateTyping(from, true)
-                const commFil = command[sr]
-                const commFile = require(`./commands/${commFil}`)
-                commFile.run(client, message, args, config)
-            }
-        }
-
-    } catch (e) {
-        console.error(e)
-        await client.reply(from, `Ocurrio un error`, id)
-    }
-}
+module.exports = async (client, message) => {
+	const { body, quotedMsg, isGroupMsg } = message;
+	if (isGroupMsg) {
+		if (mentionedJidList && mentionedJidList[0]) {
+			await getAfk(client, message);
+		}
+		await lvlFunc(client, message);
+	}
+	let q = quotedMsg ? quotedMsg.body.trim().split(" ") : undefined;
+	if (body.startsWith(config.prefix)) {
+		const arg = body.slice(config.prefix.length).trim().split(" ");
+		const cmd = arg.shift().toLowerCase();
+		const args = [arg, q];
+		const cm =
+			command.indexOf(cmd) === -1
+				? alias.indexOf(cmd)
+				: command.indexOf(cmd);
+		if (cm >= 0) {
+			const commFil = command[sr];
+			const commFile = require(`./commands/${commFil}`);
+			commFile.run(message, args);
+		}
+	}
+};
--- index.js
+++ index.js
@@ -1,59 +1,47 @@
-const { create, Client } = require("@open-wa/wa-automate")
-const handler = require("./handler")
-const temp = require("./media/temp/output.json")
+const { create, Client, ev } = require("@open-wa/wa-automate");
+const config = require("./config.json");
+const handler = require("./handler");
+const d = new Date();
 
 const start = async (client = new Client()) => {
-    console.log(`[SR] Cliente listo - ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`)
-    await client.sendText(temp.from, `Bot listo`)
-    await client.onStateChanged((state) => {
-        console.log(`[SR] ${state} - ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`)
-        if (state === 'CONFLICT' || state === 'UNLAUNCHED') {
-            client.forceRefocus()
-        }
-    })
+	console.log(
+		`[SR] Cliente listo - ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+	);
+	//Nota: Debe existir una conversacion anterior entre el numero del propietario y el numero que hosteara el bot, de otro modo, el aviso no se enviara
+	await client.sendText(`${config.owner}@c.us`, "Cliente listo");
+	await client.onStateChanged((state) => {
+		console.log(
+			`[SR] ${state} - ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+		);
+		if (state === "CONFLICT" || state === "UNLAUNCHED") {
+			client.forceRefocus();
+		}
+	});
 
-    await client.onMessage(async (message) => {
-        await handler(client, message)
-    })
-
-    await client.onAddedToGroup(async (chat) => {
-
-        client.sendText(chat.groupMetadata.id, `Gracias por la invitacion
-Puedes escribir !help para ver los comandos
-Para resolver tus dudas sobre el desarrollo del bot, puedes contactarme aqui:
-WhatsApp: wa.me/525633592644
-Discord: Cyopn#7302
-Instagram: https://instagram.com/Cyopn_`)
-
-    })
-
-    await client.onIncomingCall(async (call) => {
-        await client.sendText(call.peerJid, "Llamar = Bloquear\nPara arreglar esto contactame\nwa.me/525633592644")
-            .then(() => {
-                client.contactBlock(call.peerJid)
-            })
-    })
-}
+	await client.onMessage(async (message) => {
+		await handler(client, message);
+	});
+};
 
 create({
-    sessionId: "Cyopn",
-    headless: true,
-    qrTimeout: 0,
-    authTimeout: 0,
-    cacheEnabled: false,
-    useChrome: true,
-    killProcessOnBrowserClose: true,
-    throwErrorOnTosBlock: true,
-    chromiumArgs: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--aggressive-cache-discard',
-        '--disable-cache',
-        '--disable-application-cache',
-        '--disable-offline-load-stale-cache',
-        '--disk-cache-size=0'
-    ],
-    onError: 'AS_STRING'
+	sessionId: "Cyopn",
+	headless: true,
+	qrTimeout: 0,
+	authTimeout: 0,
+	cacheEnabled: false,
+	useChrome: true,
+	killProcessOnBrowserClose: true,
+	throwErrorOnTosBlock: true,
+	chromiumArgs: [
+		"--no-sandbox",
+		"--disable-setuid-sandbox",
+		"--aggressive-cache-discard",
+		"--disable-cache",
+		"--disable-application-cache",
+		"--disable-offline-load-stale-cache",
+		"--disk-cache-size=0",
+	],
+	onError: "AS_STRING",
 })
-    .then(client => start(client))
-    .catch(e => console.log("[SR]", e))
+	.then((client) => start(client))
+	.catch((e) => console.log(`Error al iniciar cliente: ${e.message}`));
