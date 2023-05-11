29a463739bb651dbb1179b862c158fa40667e8b7
Cyopn <2021452176@teschi.edu.mx>
2023-05-11 00:47:03 -0600

    Stickers basados en texto
    Stickers animados basados en texto

---
+++ commands/attp.js
@@ -0,0 +1,31 @@
+const { prefix } = require("../config.json");
+const axios = require("axios").default;
+
+module.exports.run = async (client, message, args) => {
+	const { id, from } = message;
+	const arg = args[1] === undefined ? args[0].join(" ") : args[1].join(" ");
+	console.log(arg);
+	if (!arg)
+		return await client.reply(
+			from,
+			`Es necesario proporcionar un texto, escribe ${prefix}attp (texto), si tienes dudas sobre este comando escribe ${prefix}help attp`,
+			id
+		);
+	const r = await axios.get(
+		`https://api.helv.io/attp?text=${encodeURIComponent(arg)}&format=base64`
+	);
+	await client.sendImageAsSticker(from, r.data, {
+		author: "ig: @Cyopn_",
+		pack: "CyopnBot",
+		keepScale: true,
+	});
+	await client.simulateTyping(from, false);
+};
+
+module.exports.config = {
+	name: `attp`,
+	alias: `ap`,
+	type: `misc`,
+	description: `Crea un sticker segun el texto proporcionado, algunos emojis no son soportados, escribiendo ${prefix}attp (texto), recuerda que no es necesario escribir los corchetes.`,
+	fulldesc: `Comando para crear stickers (pegatinas), algunos emojis no son soportados, usa este comando escribiendo ${prefix}attp (texto), o con su alias ${prefix}ap (texto), recuerda que no es necesario escribir los corchetes. \nEste comando puede usarse en mensajes directos y/o grupos.`,
+};
