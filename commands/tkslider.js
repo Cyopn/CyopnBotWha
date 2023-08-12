4211ac424b4954bbb43e2443c168ce3981940cdc
Cyopn <2021452176@teschi.edu.mx>
2023-08-12 00:06:56 -0600

    Descargador de tiktoks deslizables

---
+++ commands/tkslider.js
@@ -0,0 +1,44 @@
+const { prefix, zenKey } = require("../config.json");
+const { get } = require("axios").default;
+module.exports.run = async (sock, msg, args) => {
+	const arg = args[1] === undefined ? args[0].join("") : args[1].join("");
+	const r = await get(
+		`https://api.zahwazein.xyz/downloader/ttslide?apikey=${zenKey}&url=${arg}`,
+	);
+
+	if (r.data.status === "OK") {
+		if (r.data.result.length > 0) {
+			r.data.result.forEach((rs) => {
+				sock.sendMessage(
+					msg.key.remoteJid,
+					{ image: { url: rs }, caption: "w" },
+					{ quoted: msg },
+				);
+			});
+		} else {
+			await sock.sendMessage(
+				msg.key.remoteJid,
+				{
+					text: `El enlace no es valido o no no contiene imagenes.`,
+				},
+				{ quoted: msg },
+			);
+		}
+	} else {
+		await sock.sendMessage(
+			msg.key.remoteJid,
+			{
+				text: `El servicio no esta disponible, Intenta mas tarde.`,
+			},
+			{ quoted: msg },
+		);
+	}
+};
+
+module.exports.config = {
+	name: `tkslider`,
+	alias: `tks`,
+	type: `misc`,
+	description: `Envia algun tiktok deslizable (imagenes) sin marca de agua.`,
+	fulldesc: `Comando para descargar imagenes de un Tiktok deslizable, escribe ${prefix}tkslider (enlace), o con su alias ${prefix}tks (enlace), recuerda que no es necesario escribir los parentesis, tambien puedes responder a un enlace ya enviado, usando ${prefix}tkslider, o su alias ${prefix}tks respondiendo al enlace. \nEste comando puede usarse en mensajes directos y/o grupos.`,
+};
