d3ca91c5b5719ef7ad62f52c8f63bde50c08a9bb
Cyopn <calvillobenitezjuanantonio@gmail.com>
2023-08-26 01:29:40 -0600

    Letras de canciones

---
+++ commands/lyrics.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const { prefix, owner } = process.env;
+const { Client } = require("genius-lyrics");
+
+module.exports.run = async (sock, msg, args) => {
+	const arg =
+		args[1] === undefined && args[0].join(" ").length >= 1
+			? args[0].join(" ")
+			: args[1] === undefined
+			? ""
+			: args[1].join(" ");
+	if (!arg)
+		return sock.sendMessage(
+			msg.key.remoteJid,
+			{
+				text: `Es necesario proporcionar una busqueda, escribe ${prefix}lyrics (busqueda), recuerda que no es necesario escribir los parentesis, si tienes dudas sobre este comando escribe ${prefix}help lyrics.`,
+			},
+			{ quoted: msg },
+		);
+	try {
+		const client = new Client();
+		const [sr] = await client.songs.search(arg);
+		const rs = await sr.lyrics();
+		await sock.sendMessage(
+			msg.key.remoteJid,
+			{
+				text: `${sr.fullTitle}\n${rs}`,
+			},
+			{ quoted: msg },
+		);
+	} catch (e) {
+		await sock.sendMessage(`${owner}@s.whatsapp.net`, {
+			text: `Error en ${this.config.name} - ${
+				msg.key.remoteJid
+			}\n${String(e)}`,
+		});
+		await sock.sendMessage(
+			msg.key.remoteJid,
+			{
+				text: "Ocurrio un error inesperado.",
+			},
+			{ quoted: msg },
+		);
+	}
+};
+
+module.exports.config = {
+	name: `lyrics`,
+	alias: `ly`,
+	type: `misc`,
+	description: `Envia la letra de alguna cancion.`,
+	fulldesc: `Comando para enviar la letra de una cancion segun la busqueda proporcionada, escribe ${prefix}lyrics (busqueda), o con su alias ${prefix}ly (busqueda), recuerda que no es necesario escribir los parentesis, tambien puedes responder al titulo de una cancion, usando ${prefix}lyrics, o su alias ${prefix}ly, al responderlo. \nEste comando puede usarse en mensajes directos y/o grupos.`,
+};
