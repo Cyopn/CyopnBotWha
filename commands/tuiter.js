c85d3f330cd9b08b83764117f92c84881eb1841c
Cyopn <calvillobenitezjuanantonio@gmail.com>
2023-12-31 18:40:35 -0600

    Agregado twitter
    Corrección al comando descargador de Twitter.

---
+++ commands/tuiter.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+const { prefix, owner } = process.env;
+const { twitterdl } = require("@bochilteam/scraper");
+module.exports.run = async (sock, msg, args) => {
+	const arg =
+		args[1] === undefined && args[0].join("").length >= 1
+			? args[0].join("")
+			: args[1] === undefined
+			? ""
+			: args[1].join("");
+	if (!arg)
+		return sock.sendMessage(
+			msg.key.remoteJid,
+			{
+				text: `Es necesario proporcionar un texto, escribe ${prefix} (texto), recuerda que no es necesario escribir los parentesis, si tienes dudas sobre este comando escribe ${prefix}help.`,
+			},
+			{ quoted: msg },
+		);
+	try {
+		const response = await twitterdl(arg);
+		await sock.sendMessage(
+			msg.key.remoteJid,
+			{
+				caption: "w",
+				video: { url: response[0].url },
+			},
+			{ quoted: msg },
+		);
+	} catch (e) {
+		if (e.toString().includes("Error: No results found"))
+			return await sock.sendMessage(
+				msg.key.remoteJid,
+				{
+					text: "No se encontro el contenido, asegurate que el enlace sea correspondiente a un video.",
+				},
+				{ quoted: msg },
+			);
+		await sock.sendMessage(`${owner}@s.whatsapp.net`, {
+			text: `Error en ${this.config.name} - ${
+				msg.key.remoteJid
+			}\n${String(e)}`,
+		});
+		await sock.sendMessage(
+			msg.key.remoteJid,
+			{
+				text: "Ocurrio un error inesperado.",
+			},
+			{ quoted: msg },
+		);
+	}
+};
+
+module.exports.config = {
+	name: `tuiter`,
+	alias: `twitter`,
+	type: `misc`,
+	description: `Envia algun video de twitter.`,
+	fulldesc: `Comando para descargar videos de Twitter (X), escribe ${prefix}tuiter (enlace), o con su alias ${prefix}twitter (enlace), recuerda que no es necesario escribir los parentesis, tambien puedes responder a un enlace ya enviado, usando ${prefix}tuiter, o su alias ${prefix}twitter respondiendo al enlace. \nEste comando puede usarse en mensajes directos y/o grupos.`,
+};
