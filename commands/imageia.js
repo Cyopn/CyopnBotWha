ac741bd0728f76f36f865ca509a823589cd02446
Cyopn <77410038+Cyopn@users.noreply.github.com>
2024-02-11 00:52:12 -0600

    Nuevos comandos

---
+++ commands/imageia.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+const { prefix, owner } = process.env;
+const axios = require("axios").default;
+
+module.exports.run = async (sock, msg, args) => {
+	const arg =
+		args[1] === undefined && args[0].join(" ").length >= 1
+			? args[0].join(" ")
+			: args[1] === undefined
+			? ""
+			: args[1].join(" ");
+	if (!arg)
+		return await sock.sendMessage(
+			msg.key.remoteJid,
+			{
+				text: `Es necesario proporcionar un texto, escribe ${prefix}imageia (texto), recuerda que no es necesario escribir los parentesis, si tienes dudas sobre este comando escribe ${prefix}help imageia.`,
+			},
+			{ quoted: msg },
+		);
+	try {
+		const response = await axios.get(
+			`https://aemt.me/ai/text2img?text=${encodeURI(arg)}`,
+			{
+				responseType: "arraybuffer",
+			},
+		);
+		await sock.sendMessage(
+			msg.key.remoteJid,
+			{
+				caption: "w",
+				image: response.data,
+			},
+			{ quoted: msg },
+		);
+	} catch (e) {
+		await sock.sendMessage(`${owner}@s.whatsapp.net`, {
+			text: `Error en ${this.config.name} - ${
+				msg.key.remoteJid
+			}\n${String(e)}`,
+		});
+		await sock.sendMessage(
+			msg.key.remoteJid,
+			{
+				text: "Ocurrio un error inesperado.",
+			},
+			{ quoted: msg },
+		);
+	}
+};
+
+module.exports.config = {
+	name: `imageia`,
+	alias: `ima`,
+	type: `misc`,
+	description: `Genera imagenes con inteligencia artificial.`,
+	fulldesc: `Comando para generar imagenes con inteligencia artificial, escribe ${prefix}imageia (texto), o con su alias ${prefix}ima (testo), recuerda que no es necesario escribir los parentesis, tambien puedes responder a un texto ya enviado, usando ${prefix}imageia, o su alias ${prefix}ima respondiendo al texto. \nEste comando puede usarse en mensajes directos y/o grupos..`,
+};
---
+++ commands/stelegram.js
@@ -0,0 +1,111 @@
+require("dotenv").config();
+const { prefix, owner, token } = process.env;
+const axios = require("axios").default;
+const { tgsConverter, sticker } = require("../lib/functions.js");
+
+module.exports.run = async (sock, msg, args) => {
+	const arg =
+		args[1] === undefined && args[0].join("").length >= 1
+			? args[0].join("")
+			: args[1] === undefined
+			? ""
+			: args[1].join("");
+	if (!arg)
+		return await sock.sendMessage(
+			msg.key.remoteJid,
+			{
+				text: `Es necesario proporcionar un enlace, escribe ${prefix}stelegram (texto), recuerda que no es necesario escribir los parentesis, si tienes dudas sobre este comando escribe ${prefix}help stelegram.`,
+			},
+			{ quoted: msg },
+		);
+	try {
+		if (!arg.includes("https://t.me/addstickers/"))
+			return await sock.sendMessage(
+				msg.key.remoteJid,
+				{
+					text: `El enlace proporcionado no es valido, recuerda que el enlace debe ser directo del paquete de stickers.`,
+				},
+				{ quoted: msg },
+			);
+		const pack = arg.replace("https://t.me/addstickers/", "");
+		const response = await axios.get(
+			`https://api.telegram.org/bot${token}/getStickerSet?name=${pack}`,
+		);
+		if (response.data.ok) {
+			const stickers = response.data.result.stickers;
+			await sock.sendMessage(
+				msg.key.remoteJid,
+				{
+					text: `Se he encontrado el paquete de ${pack} con ${stickers.length} stickers, el envio puede demorar un poco, espera un momento.`,
+				},
+				{ quoted: msg },
+			);
+			for (const s of stickers) {
+				const file = await axios.get(
+					`https://api.telegram.org/bot${token}/getFile?file_id=${s.file_id}`,
+				);
+				const st = await axios.get(
+					`https://api.telegram.org/file/bot${token}/${file.data.result.file_path}`,
+					{ responseType: "arraybuffer" },
+				);
+				const url = await tgsConverter(st.data);
+				const buffer = await axios.get(url, {
+					responseType: "arraybuffer",
+				});
+				const result = await sticker(buffer.data).catch((e) => {
+					sock.sendMessage(`${owner}@s.whatsapp.net`, {
+						text: `Error en ${this.config.name0000} - ${
+							msg.key.remoteJid
+						}\n${String(e)}`,
+					});
+					sock.sendMessage(
+						msg.key.remoteJid,
+						{
+							text: "Ocurrio un error inesperado.",
+						},
+						{ quoted: msg },
+					);
+				});
+				await sock
+					.sendMessage(
+						msg.key.remoteJid,
+						{ sticker: result },
+						{ quoted: msg },
+					)
+					.catch((e) => {
+						sock.sendMessage(`${owner}@s.whatsapp.net`, {
+							text: String(e),
+						});
+						sock.sendMessage(
+							msg.key.remoteJid,
+							{
+								text: "Ocurrio un error inesperado.",
+							},
+							{ quoted: msg },
+						);
+					});
+			}
+		}
+	} catch (e) {
+		await sock.sendMessage(`${owner}@s.whatsapp.net`, {
+			text: `Error en ${this.config.name} - ${
+				msg.key.remoteJid
+			}\n${String(e)}`,
+		});
+		await sock.sendMessage(
+			msg.key.remoteJid,
+			{
+				text: "Ocurrio un error inesperado.",
+			},
+			{ quoted: msg },
+		);
+	}
+};
+
+module.exports.config = {
+	name: `stelegram`,
+	alias: `st`,
+	type: `misc`,
+	description: `Envia stickers de un paquete de stickers de Telegram.`,
+	fulldesc: `Este comando envia stickers de un paquete de stickers de Telegram, para usarlo escribe ${prefix}stelegram (enlace), recuerda que no es necesario escribir los parentesis, si tienes dudas sobre este comando escribe ${prefix}help stelegram.`,
+};
