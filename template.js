871a504e9d26206d76e7af23fe4007fac46ba003
Cyopn <77410038+Cyopn@users.noreply.github.com>
2024-12-19 23:10:36 -0600

    creado (plantilla de comandos)

---
+++ template.js
@@ -0,0 +1,28 @@
+//Importaciones de .
+require("dotenv").config();
+const { prefix, owner } = process.env;
+const { errorHandler } = require("../lib/functions");
+
+module.exports.run = async (sock, msg, args) => {
+    //Argumetos recibidos junto al comando
+    const arg =
+        args[1] === undefined && args[0].join(" ").length >= 1
+            ? args[0].join(" ")
+            : args[1] === undefined
+                ? ""
+                : args[1].join(" ");
+    try {
+        //Codigo del comando
+    } catch (e) {
+        await errorHandler(sock, msg, this.config.name, e);
+    }
+};
+
+module.exports.config = {
+    //Informacion sobre el comando
+    name: ``,
+    alias: ``,
+    type: ``,
+    description: ``,
+    fulldesc: ``,
+};
